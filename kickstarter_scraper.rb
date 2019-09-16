d2058724916834cde91fe2c90880cc0a80bb9e83
assane savadogo <assane.savadogo81@gmail.com>
2019-09-16 21:55:21 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,14 +2,19 @@
 require 'nokogiri'
 require 'pry'
 
+ 
 def create_project_hash
+<<<<<<< HEAD
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
  
+=======
+>>>>>>> 84e5c4f50bff605a3e9aabe99a78c6cbea8c8a33
   projects = {}
  
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
+<<<<<<< HEAD
     projects[title.to_sym] = {
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
       :description => project.css("p.bbcard_blurb").text,
@@ -20,3 +25,12 @@ def create_project_hash
   end
   projects
 end
+=======
+    projects[title.to_sym] = {}
+    binding.pry
+  end
+  projects
+end
+# create_project_hash
+# project.css(".location-name").text
+>>>>>>> 84e5c4f50bff605a3e9aabe99a78c6cbea8c8a33
